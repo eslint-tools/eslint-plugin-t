396b76fe48044bdcb0672c728b57fa70e0da4986
Simon <sstur@me.com>
2022-06-04 10:29:43 -0700

    Remove array-literal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const stringLiteralRule = require('./string-literal');
-const arrayLiteralRule = require('./array-literal');
 
 const rules = {
   'string-literal': stringLiteralRule,
-  'array-literal': arrayLiteralRule,
 };
 
 module.exports = { rules };
